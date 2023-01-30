77ea367cc54b1339f35364dffe05f831a763e239
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-30 12:36:07 +0000

    changes

---
+++ upgraded_user_details.py
@@ -0,0 +1,8 @@
+print("Hi, what is your username?")
+username = input()
+print("How old are you?")
+age = input()
+print("What is you DOB?")
+DOB = input()
+
+print(f"Hi {username.capitalize()} you are {age} years old and your date of birth is {DOB}")
