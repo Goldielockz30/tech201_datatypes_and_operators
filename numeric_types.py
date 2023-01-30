b105a7688d95bbfb9629987efbd5fb76c887e936
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-30 12:29:29 +0000

    changes

--- numeric_types.py
+++ numeric_types.py
@@ -25,20 +25,20 @@
 
 # Ints
 
-a = 24
-b = 16
-
-print(a + b) # 40
-print(a > b) # True
-print(a < b) # False
+# a = 24
+# b = 16
+#
+# print(a + b) # 40
+# print(a > b) # True
+# print(a < b) # False
 
 # Floats
-FloatNum = 1.356
-IntNum = 4
-print(FloatNum + IntNum)
+# FloatNum = 1.356
+# IntNum = 4
+# print(FloatNum + IntNum)
 
 # Theres no limit to the size of decimal, but python will round things up
 
-one_third = 1 / 3
-print(one_third) # 0.3333333333333333
-print(one_third * 3) # 1.0
+# one_third = 1 / 3
+# print(one_third) # 0.3333333333333333
+# print(one_third * 3) # 1.0
